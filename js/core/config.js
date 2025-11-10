3170d753bfe44a03d4ac6c8d3c4fe9495f479a60
caesirian <hernan.garbarino@gmail.com>
2025-11-10 18:40:59 -0300

    Create config.js

---
+++ js/core/config.js
@@ -0,0 +1,14 @@
+const CONFIG = {
+  GAS_ENDPOINT: "https://script.google.com/macros/s/AKfycbyTLrGiqes1VOp8E51Kq91P7YihPKNtN1f0f6u2JitXOteGyGrNXqiSjdXYJ8HAtfs/exec",
+  CACHE_KEY: "hg_dashboard_cache_v4",
+  CACHE_TTL: 1000 * 60 * 60,
+  COMPONENTES_ACTIVOS: [
+    'saldoCaja',
+    'ingresosVsEgresos',
+    'egresosVsAnterior',
+    'cuentasPendientes',
+    'analisisCategorias',
+    'cotizacionesMonedas',
+    'controlStock'
+  ]
+};
