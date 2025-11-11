f3472b84ae0f26765d8e43e8b837e4f30d0e6340
caesirian <hernan.garbarino@gmail.com>
2025-11-11 08:04:37 -0300

    Update config.js

--- js/core/config.js
+++ js/core/config.js
@@ -1,31 +1,19 @@
 // config.js
 const CONFIG = {
   GAS_ENDPOINT: "https://script.google.com/macros/s/AKfycbyTLrGiqes1VOp8E51Kq91P7YihPKNtN1f0f6u2JitXOteGyGrNXqiSjdXYJ8HAtfs/exec",
-  CACHE_KEY: "hg_dashboard_cache_v4",
+  CACHE_KEY: "hg_dashboard_cache_v5",
   CACHE_TTL: 1000 * 60 * 60,
   
-  // Componentes categorizados por tamaño/prioridad
-  COMPONENTES: {
-    // Componentes livianos (siempre cargan primero)
-    livianos: [
-      'saldoCaja',
-      'ingresosVsEgresos', 
-      'egresosVsAnterior',
-      'cotizacionesMonedas'
-    ],
-    
-    // Componentes medianos (carga secundaria)
-    medianos: [
-      'analisisCategorias',
-      'cuentasPendientes'
-    ],
-    
-    // Componentes pesados (carga bajo demanda)
-    pesados: [
-      'controlStock',
-      'proyeccionFlujo'
-    ]
-  },
+  // Componentes disponibles
+  COMPONENTES_DISPONIBLES: [
+    'saldoCaja',
+    'ingresosVsEgresos',
+    'egresosVsAnterior', 
+    'cotizacionesMonedas',
+    'analisisCategorias',
+    'cuentasPendientes',
+    'controlStock'
+  ],
   
   // Configuración por defecto (qué componentes mostrar inicialmente)
   COMPONENTES_POR_DEFECTO: {
@@ -35,7 +23,6 @@ const CONFIG = {
     cotizacionesMonedas: true,
     analisisCategorias: false,
     cuentasPendientes: false,
-    controlStock: false,
-    proyeccionFlujo: false
+    controlStock: false
   }
 };
