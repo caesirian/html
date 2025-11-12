4e29767ecf30ecc9f6f15dae3f2cf696a949aec6
caesirian <hernan.garbarino@gmail.com>
2025-11-12 07:11:40 -0300

    Update config.js

--- js/core/config.js
+++ js/core/config.js
@@ -1,34 +1,19 @@
 // config.js
 const CONFIG = {
   GAS_ENDPOINT: "https://script.google.com/macros/s/AKfycbyTLrGiqes1VOp8E51Kq91P7YihPKNtN1f0f6u2JitXOteGyGrNXqiSjdXYJ8HAtfs/exec",
-  CACHE_KEY: "hg_dashboard_cache_v5",
+  CACHE_KEY: "hg_dashboard_cache_v6",
   CACHE_TTL: 1000 * 60 * 60,
   
-  // Componentes disponibles
-  COMPONENTES_DISPONIBLES: [
-    'saldoCaja',
-    'ingresosVsEgresos',
-    'egresosVsAnterior', 
-    'cotizacionesMonedas',
-    'analisisCategorias',
-    'cuentasPendientes',
-        'proyeccionFlujo',
-        'calculadoraInversion',
-        'calculadoraInversiones',
-    'controlStock'
-  ],
+  // Estos ahora se derivan del registro
+  get COMPONENTES_DISPONIBLES() {
+    return Object.keys(ComponentsRegistry.getAll());
+  },
   
-  // Configuración por defecto (qué componentes mostrar inicialmente)
-  COMPONENTES_POR_DEFECTO: {
-    saldoCaja: true,
-    ingresosVsEgresos: true,
-    egresosVsAnterior: true,
-    cotizacionesMonedas: true,
-    analisisCategorias: false,
-    cuentasPendientes: false,
-    proyeccionFlujo:true,
-    calculadoraInversion:true,
-    calculadoraInversiones:true,
-    controlStock: false
+  get COMPONENTES_POR_DEFECTO() {
+    const defaultActive = ComponentsRegistry.getDefaultActive();
+    return Object.keys(ComponentsRegistry.getAll()).reduce((acc, id) => {
+      acc[id] = defaultActive[id] ? true : false;
+      return acc;
+    }, {});
   }
 };
